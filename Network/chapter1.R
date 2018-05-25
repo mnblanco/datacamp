8710987586481105111add3d8f1d6645f07ad877
Marjorie Blanco <marjorie.n.blanco@boeing.com>
2018-05-25 13:38:50 -0700

    chapter 1 commit

--- Network/chapter1.R
+++ Network/chapter1.R
@@ -26,3 +26,100 @@ gsize(g)
 
 # Count number of vertices
 gorder(g)
+
+
+genders <- c("M","F","F","M","M","M","F","M","M","F","M","F","M","F","M","M")
+ages <- c(18,19,21,20,22,18,23,21,22,20,20,22,21,18,19,20)
+
+# Inspect the objects 'genders' and 'ages'
+genders
+ages
+
+# Create new vertex attribute called 'gender'
+g <- set_vertex_attr(g, "gender", value = genders)
+
+# Create new vertex attribute called 'age'
+g <- set_vertex_attr(g, "age", value = ages)
+
+# View all vertex attributes in a list
+vertex_attr(g)
+
+# View attributes of first five vertices in a dataframe
+V(g)[[1:5]] 
+
+
+hours <-  c(1,2,2,1,2,5,5,1,1,3,2,1,1,5,1,2,4,1,3,1,1,1,4,1,3,3,4)
+
+
+
+# View hours
+hours
+
+# Create new edge attribute called 'hours'
+g <- set_edge_attr(g, "hours", value = hours)
+
+# View edge attributes of graph object
+edge_attr(g)
+
+# Find all edges that include "Britt"
+E(g)[[inc('Britt')]]  
+
+# Find all pairs that spend 4 or more hours together per week
+E(g)[[hours>=4]]  
+
+
+# Create an igraph object with attributes directly from dataframes
+g1 <- graph_from_data_frame(d = friends1_edges, vertices = friends1_nodes, directed = FALSE)
+
+
+# Subset edges greater than or equal to 5 hours
+E(g1)[[hours >= 5]]  
+
+# Plot network and color vertices by gender
+V(g1)$color <- ifelse(V(g1)$gender == "females", "orange", "dodgerblue")
+plot(g1, vertex.label.color = "black")
+
+
+# Plot the graph object g1 in a circle layout
+plot(g1, vertex.label.color = "black", layout = layout_in_circle(g1))
+
+# Plot the graph object g1 in a Fruchterman-Reingold layout 
+plot(g1, vertex.label.color = "black", layout = layout_with_fr(g1))
+
+# Plot the graph object g1 in a Tree layout 
+m <- layout_as_tree(g1)
+plot(g1, vertex.label.color = "black", layout = m)
+
+# Plot the graph object g1 using igraph's chosen layout 
+m1 <- layout_nicely(g1)
+plot(g1, vertex.label.color = "black", layout = m1)
+
+
+# Create a vector of weights based on the number of hours each pair spend together
+w1 <- E(g1)$hours
+
+# Plot the network varying edges by weights
+m1 <- layout_nicely(g1)
+plot(g1, 
+     vertex.label.color = "black", 
+     edge.color = 'black',
+     edge.width = w1,
+     layout = m1)
+
+
+# Create a new igraph object only including edges from the original graph that are greater than 2 hours long 
+g2 <- delete_edges(g1, E(g1)[hours < 2])
+
+
+# Plot the new graph 
+w2 <- E(g2)$hours
+m2 <- layout_nicely(g2)
+
+plot(g2, 
+     vertex.label.color = "black", 
+     edge.color = 'black',
+     edge.width = w2,
+     layout = m2)
+
+
+E(g1)[[inc('Jasmine')]]
