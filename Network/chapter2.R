c9aabf074024dc47686a87f983b1a80e87946903
Marjorie Blanco <marjorie.n.blanco@boeing.com>
2018-05-25 13:39:08 -0700

    new chapter 2 code

---
+++ Network/chapter2.R
@@ -0,0 +1,114 @@
+
+# Load igraph
+library(igraph)
+
+friends <- read.csv("~/datacamp/Network/measles.csv", header=FALSE)
+
+# Get the graph object
+g <- graph_from_data_frame(measles, directed = TRUE)
+
+# is the graph directed?
+is.directed(g)
+
+# Is the graph weighted?
+is.weighted(g)
+
+# Where does each edge originate from?
+table(head_of(g, E(g)))
+
+# Make a basic plot
+plot(g, 
+     vertex.label.color = "black", 
+     edge.color = 'gray77',
+     vertex.size = 0,
+     edge.arrow.size = 0.1,
+     layout = layout_nicely(g))
+
+# Is there an edge going from vertex 184 to vertex 178?
+g['184', '178']
+
+# Is there an edge going from vertex 178 to vertex 184?
+g['178', '184']
+
+# Show all edges going to or from vertex 184
+incident(g, '184', mode = c("all"))
+
+# Show all edges going out from vertex 184
+incident(g, '184', mode = c("out"))
+
+
+
+# Identify all neighbors of vertex 12 regardless of direction
+neighbors(g, '12', mode = c('all'))
+
+# Identify other vertices that direct edges towards vertex 12
+neighbors(g, '12', mode = c('in'))
+
+# Identify any vertices that receive an edge from vertex 42 and direct an edge to vertex 124
+n1 <- neighbors(g, '42', mode = c('out'))
+n2 <- neighbors(g, '124', mode = c('in'))
+intersection(n1, n2)
+
+
+# Which two vertices are the furthest apart in the graph ?
+farthest_vertices(g) 
+
+# Shows the path sequence between two furthest apart vertices.
+get_diameter(g)  
+
+# Identify vertices that are reachable within two connections from vertex 42
+ego(g, 2, '42', mode = c('out'))
+
+# Identify vertices that can reach vertex 42 within two connections
+ego(g, 2, '42', mode = c('in'))
+
+
+# Calculate the out-degree of each vertex
+g.outd <- degree(g, mode = c("out"))
+
+# View a summary of out-degree
+table(g.outd)
+
+# Make a histogram of out-degrees
+hist(g.outd, breaks = 30)
+
+# Find the vertex that has the maximum out-degree
+which.max(g.outd)
+
+# Calculate betweenness of each vertex
+g.b <- betweenness(g, directed = TRUE)
+
+# Show histogram of vertex betweenness
+hist(g.b, breaks = 80)
+
+# Create plot with vertex size determined by betweenness score
+plot(g, 
+     vertex.label = NA,
+     edge.color = 'black',
+     vertex.size = sqrt(g.b)+1,
+     edge.arrow.size = 0.05,
+     layout = layout_nicely(g))
+
+
+# Make an ego graph
+g184 <- make_ego_graph(g, diameter(g), nodes = '184', mode = c("all"))[[1]]
+
+# Get a vector of geodesic distances of all vertices from vertex 184 
+dists <- distances(g184, "184")
+
+# Create a color palette of length equal to the maximal geodesic distance plus one.
+colors <- c("black", "red", "orange", "blue", "dodgerblue", "cyan")
+
+# Set color attribute to vertices of network g184.
+V(g184)$color <- colors[dists+1]
+
+# Visualize the network based on geodesic distance from vertex 184 (patient zero).
+plot(g184, 
+     vertex.label = dists, 
+     vertex.label.color = "white",
+     vertex.label.cex = .6,
+     edge.color = 'black',
+     vertex.size = 7,
+     edge.arrow.size = .05,
+     main = "Geodesic Distances from Patient Zero"
+)
